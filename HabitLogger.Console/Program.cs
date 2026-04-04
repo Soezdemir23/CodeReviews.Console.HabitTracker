f7680762b6de623644cd2f626cf80782863bb0b9
Soezdemir23 <soezdemir3423@gmail.com>
2026-04-04 13:38:35 +0000

    feat: implement initial database connection setup in Program.cs and rename test class to DatabaseTests

--- HabitLogger.Console/Program.cs
+++ HabitLogger.Console/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+var databasePath = Path.Join(Directory.GetCurrentDirectory(), "data");
+var databasePathWithDatabase = Path.Join(databasePath, "habitLogger.db");
+
+// create a connection or establish a connection
+string connectionString = $"Data Source={databasePath}";
+
+// a Test to see if I can query anything at all:
+
--- HabitLogger.Tests/UnitTest1.cs
+++ HabitLogger.Tests/UnitTest1.cs
@@ -1,15 +1,6 @@
 ﻿namespace HabitLogger.Tests;
 
-public class Tests
+public class DatabaseTests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
 }
